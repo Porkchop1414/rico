06609f0b90c09700296d9560bf58f4cceced07a1
Alex Wright <aawqkc@mst.edu>
2013-10-16 15:47:30 -0500

    Added a comment with the overview of the project

--- src/com/indigenous/Driver.java
+++ src/com/indigenous/Driver.java
@@ -1,5 +1,23 @@
 package com.indigenous;
 
+/* PROJECT OVERVIEW:
+Program Flow
+1) Read in ARFF
+2) Find coverings (efficiently)
+3) Reduce (if needed)
+4) Output
+
+Initial Focus
+1) ARFF Reading
+2) Storage of data
+3) Storage of coverings/rules
+
+Testing
+1) Demonstrate Soundness
+2) small data set verification
+*/
+
+
 public class Driver {
   public static void main(String args[]) {
     System.out.println("Rule Induction From Coverings");
